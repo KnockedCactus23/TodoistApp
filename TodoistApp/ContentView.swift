93f78cc320fd304b295f6e5bb47fc8b76785fe15
Sergio <23checo06@gmail.com>
2025-10-22 08:46:25 -0600

    Initial Commit

---
+++ TodoistApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoistApp
+//
+//  Created by Sergio Rodríguez Pérez on 22/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoistApp/TodoistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoistAppApp.swift
+//  TodoistApp
+//
+//  Created by Sergio Rodríguez Pérez on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
