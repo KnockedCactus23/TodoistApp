3f01d3f485c67f2ad423147b4978baf483e453cc
Sergio <23checo06@gmail.com>
2025-10-24 16:20:47 -0600

    App Terminada

---
+++ TodoistApp/TaskDateFormatter.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDateFormatter.swift
+//  TodoistApp
+//
+//  Created by Sergio Rodríguez Pérez on 24/10/25.
+//
+
+import SwiftUI
+
+struct TaskDateFormatter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDateFormatter()
+}
---
+++ TodoistAppTests/TodoistAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TodoistAppTests.swift
+//  TodoistAppTests
+//
+//  Created by Sergio Rodríguez Pérez on 24/10/25.
+//
+
+import Testing
+
+struct TodoistAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
