3cdb2830bdf2134e874e063e8957759bfd647974
Sergio <23checo06@gmail.com>
2025-10-23 20:17:01 -0600

    Task, TaskCreateView y TaskHomeView

---
+++ TodoistApp/TaskCreateView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCreateView.swift
+//  TodoistApp
+//
+//  Created by Sergio Rodríguez Pérez on 23/10/25.
+//
+
+import SwiftUI
+
+struct TaskCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskCreateView()
+}
